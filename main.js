b53a006ce8e29417c4b54c750e6e5a1d1aed4500
Siya117 <78736374+Siya117@users.noreply.github.com>
2021-05-02 19:52:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+player_heigth = 50;
+player_width = 50;
+
+player_x = 10;
+player_y = 10;
+
+var player_object = "";
+var block_image = "";
+
+function player_update()
+{
+    fabric.Image.fromURL("player.png", function(Img){
+
+        player_object = Img;
+        player_object.scaleToWidth(150); 
+        player_object.scaleToHeight(140);
+        player_i.set({
+            top:player_y,
+            left:player_x
+        });
+
+        canvas.add(player_image);
+
+    })
+}
+
+function new_image(get_image)
+{
+    fabric.Image.fromURL(get_image, function(Img){
+
+        block_image = Img;
+        block_image.scaleToWidth(block_width); 
+        block_image.scaleToHeight(block_heigth);
+        block_image.set({
+            top:player_y,
+            left:player_x
+        });
+
+        canvas.add(block_image);
+        
+    })
+}
